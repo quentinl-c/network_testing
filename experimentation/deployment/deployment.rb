2d46f842af80992a5585c26ada7d5417a2a84ecb
quentinl-c <quentin.laportechabasse@linagora.com>
2016-06-17 18:57:47 +0200

    Improve deployment script and fix bugs

--- experimentation/deployment/deployment.rb
+++ experimentation/deployment/deployment.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 require 'distem'
+require 'net/http'
+require 'json'
 
 FSRABBITMQ = 'file:///home/qlaportechabasse/rabbimq-lxc.tar.gz'
 FSSERVER = 'file:///home/qlaportechabasse/server-lxc.tar.gz'
@@ -7,7 +9,8 @@ FSCLIENT = 'file:///home/qlaportechabasse/client-lxc.tar.gz'
 GATEWAY  = '10.147.255.254' # Default gateway of Nancy's sites
 DOCID = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
 MUTEHOST = 'http://mute-collabedition.rhcloud.com/peer/doc/'
-
+SERVER_PORT = 5000
+SAVE_DIR = '/tmp/'
 
 vnet = {
   'name' => 'expnet',
@@ -23,12 +26,12 @@ sshkeys = {
 }
 
 server = {
-    'name' => 'server3',
+    'name' => 'server',
     'address' => nil
 }
 
 rabbitmq = {
-    'name' => 'rabbit-mq3',
+    'name' => 'rabbit-mq',
     'address' => nil
 }
 
@@ -36,12 +39,13 @@ rabbitmq = {
 ifname = 'if0'
 
 # Experimentation's settings
-writers = 5
-readers = 2
+writers = 1
+readers = 1
 typing_speed = 5 # unit word/sec
-duration = 240 # time unit : seconde
+duration = 10 # time unit : seconde
 nbr_clients = writers + readers
 clients = []
+
 Distem.client do |cl|
     pnodes = cl.pnodes_info
     pnodes_list = pnodes.map{ |p| p[0]}
@@ -50,7 +54,7 @@ Distem.client do |cl|
     puts pnodes_list
 
     puts "=== Network ==="
-    # cl.vnetwork_create(vnet['name'], vnet['address'])
+    cl.vnetwork_create(vnet['name'], vnet['address'])
 
     puts "=== Rabbit-mq server is being lauched ==="
     cl.vnode_create(rabbitmq['name'],  { 'host' => pnodes_list[0] }, sshkeys)
@@ -102,14 +106,16 @@ Distem.client do |cl|
     # Compute the number of clients per physical machine
     ratio = nbr_clients.to_f / pnodes_list.length
     ratio = ratio > 1 ? ratio.ceil : 1
-
+    puts ratio
     # Deployment of clients
     pnodes_index = 0
     clients.each_with_index do |client, index|
-        puts "=== Client n° #{index} is being launched on #{pnodes_list[pnodes_index]} ==="
-        if index != 0 && index + 1 % ratio == 0 && pnodes_index < pnodes_list.length then
+        if index != 0 && (index + 1) % ratio == 0 && pnodes_index < pnodes_list.length then
             pnodes_index = pnodes_index + 1
         end
+
+        puts "=== #{client['name']} is being launched on #{pnodes_list[pnodes_index]} ==="
+
         cl.vnode_create(client['name'], { 'host' => pnodes_list[pnodes_index] }, sshkeys)
         cl.vfilesystem_create(client['name'], { 'image' => FSCLIENT })
         cl.viface_create(client['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true' })
@@ -118,6 +124,7 @@ Distem.client do |cl|
 
         client['address'] = cl.viface_info(client['name'], ifname)['address'].split('/')[0]
         puts "=== #{client['name']} is hosted by vnode : #{client['address']} ==="
+
         cl.vnode_execute(client['name'], "ifconfig if0 #{client['address']} netmask 255.252.0.0")
         cl.vnode_execute(client['name'], "route add default gw #{GATEWAY} dev #{ifname}")
         cl.vnode_execute(client['name'], "rm /etc/resolv.conf && cp /home/resolv.conf /etc/")
@@ -125,6 +132,42 @@ Distem.client do |cl|
         cl.vnode_execute(client['name'], "export SERVER_ADDRESS=#{server['address']} && \
                 export RABBITMQ_ADDRESS=#{rabbitmq['address']} && \
                 /etc/init.d/client start")
+
+    end
+    puts "=== Deployment completed ==="
+
+    uri = URI("http://#{server['address']}:#{SERVER_PORT}/status")
+    is_exp_started = false
+
+    puts "=== Waiting for the beginning of the experimentation ==="
+
+    while !is_exp_started
+        sleep(1)
+        puts "=== Query the server ==="
+        res = Net::HTTP.get_response(uri)
+        content = JSON.parse(res.body)
+        is_exp_started = content['details']['is_exp_started']
+        puts "=== Status of the experimentation : #{is_exp_started} ==="
+        puts content
     end
 
+    puts "=== The expreimentation should last #{duration} secondes"
+    sleep(duration)
+    is_exp_ended = false
+
+    puts "=== Waiting for the end of the experimentation ==="
+
+    while !is_exp_ended
+        sleep(1)
+        puts "=== Query the server ==="
+        res = Net::HTTP.get_response(uri)
+        content = JSON.parse(res.body)
+        is_exp_ended = content['details']['is_exp_ended']
+        puts "=== Status of the experimentation : #{is_exp_ended} ==="
+        puts content
+    end
+
+    puts "=== Recovering of results ==="
+    `scp root@#{server['address']}:/home/* #{SAVE_DIR}`
+    puts "=== Results are saved in #{SAVE_DIR} ==="
 end
