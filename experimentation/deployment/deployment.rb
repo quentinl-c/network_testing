3fc3e152f6b258047fba65590c27899bf0e434ff
quentinl-c <qlaportechabasse@linagora.com>
2016-07-28 16:32:55 +0200

    Updated scripts

--- experimentation/deployment/deployment.rb
+++ experimentation/deployment/deployment.rb
@@ -2,6 +2,7 @@
 require 'distem'
 require 'net/http'
 require 'json'
+require 'timeout'
 
 FSRABBITMQ = 'file:///home/qlaportechabasse/rabbimq-lxc.tar.gz'
 FSSERVER = 'file:///home/qlaportechabasse/server-lxc.tar.gz'
@@ -39,12 +40,18 @@ rabbitmq = {
 ifname = 'if0'
 
 # Experimentation's settings
-writers = 1
+writers = 19
 readers = 1
 typing_speed = 5 # unit word/sec
-duration = 10 # time unit : seconde
+duration = 3600 # time unit : seconde
 nbr_clients = writers + readers
 clients = []
+
+puts "=== Configuration ==="
+puts " * writers : #{writers}"
+puts " * reader : #{readers}"
+puts " * typing speed : #{typing_speed}"
+puts " * duration : #{duration} secondes"
 
 Distem.client do |cl|
     pnodes = cl.pnodes_info
@@ -53,13 +60,13 @@ Distem.client do |cl|
     puts "=== Pnodes ==="
     puts pnodes_list
 
-    puts "=== Network ==="
+    puts "=== Network will be created ==="
     cl.vnetwork_create(vnet['name'], vnet['address'])
 
     puts "=== Rabbit-mq server is being lauched ==="
     cl.vnode_create(rabbitmq['name'],  { 'host' => pnodes_list[0] }, sshkeys)
     cl.vfilesystem_create(rabbitmq['name'], { 'image' => FSRABBITMQ })
-    cl.viface_create(rabbitmq['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true' })
+    cl.viface_create(rabbitmq['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true', 'output' =>{"bandwidth" =>{"rate" => "1mbps" } }})
 
     cl.vnode_start(rabbitmq['name'])
 
@@ -74,13 +81,14 @@ Distem.client do |cl|
     puts "=== Server is being lauched ==="
     cl.vnode_create(server['name'],  { 'host' => pnodes_list[1] }, sshkeys)
     cl.vfilesystem_create(server['name'], { 'image' => FSSERVER })
-    cl.viface_create(server['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true' })
+    cl.viface_create(server['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true', 'output' =>{"bandwidth" =>{"rate" => "1mbps" }}})
     cl.vnode_start(server['name'])
     cl.wait_vnodes('vnodes'=>[server['name']])
 
     server['address'] = cl.viface_info(server['name'], ifname)['address'].split('/')[0]
     puts server['address']
 
+    cl.vnode_execute(server['name'], "bash /home/sync_clock.sh")
     cl.vnode_execute(server['name'], "ifconfig if0 #{server['address']} netmask 255.252.0.0")
     cl.vnode_execute(server['name'], "route add default gw #{GATEWAY} dev #{ifname}")
 
@@ -103,28 +111,30 @@ Distem.client do |cl|
         clients.push(client)
     end
 
-    # Compute the number of clients per physical machine
+    # Computes the number of clients per physical machine
     ratio = nbr_clients.to_f / pnodes_list.length
     ratio = ratio > 1 ? ratio.ceil : 1
-    puts ratio
+    puts "=== ratio : #{ratio}==="
     # Deployment of clients
     pnodes_index = 0
     clients.each_with_index do |client, index|
-        if index != 0 && (index + 1) % ratio == 0 && pnodes_index < pnodes_list.length then
+
+        if index != 0 && (index + 1) % ratio == 0 && pnodes_index < ((pnodes_list.length) -1) then
             pnodes_index = pnodes_index + 1
         end
-
+        puts "=== pnodes_index : #{pnodes_index}==="
         puts "=== #{client['name']} is being launched on #{pnodes_list[pnodes_index]} ==="
 
         cl.vnode_create(client['name'], { 'host' => pnodes_list[pnodes_index] }, sshkeys)
         cl.vfilesystem_create(client['name'], { 'image' => FSCLIENT })
-        cl.viface_create(client['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true' })
+        cl.viface_create(client['name'], ifname, { 'vnetwork' => vnet['name'], 'default' => 'true', 'output' =>{"bandwidth" =>{"rate" => "1mbps" }}})
         cl.vnode_start(client['name'])
         cl.wait_vnodes('vnodes'=>[client['name']])
 
         client['address'] = cl.viface_info(client['name'], ifname)['address'].split('/')[0]
         puts "=== #{client['name']} is hosted by vnode : #{client['address']} ==="
 
+        cl.vnode_execute(client['name'], "/etc/init.d/ntp start")
         cl.vnode_execute(client['name'], "ifconfig if0 #{client['address']} netmask 255.252.0.0")
         cl.vnode_execute(client['name'], "route add default gw #{GATEWAY} dev #{ifname}")
         cl.vnode_execute(client['name'], "rm /etc/resolv.conf && cp /home/resolv.conf /etc/")
@@ -141,14 +151,23 @@ Distem.client do |cl|
 
     puts "=== Waiting for the beginning of the experimentation ==="
 
-    while !is_exp_started
-        sleep(1)
-        puts "=== Query the server ==="
-        res = Net::HTTP.get_response(uri)
-        content = JSON.parse(res.body)
-        is_exp_started = content['details']['is_exp_started']
-        puts "=== Status of the experimentation : #{is_exp_started} ==="
-        puts content
+    Timeout::timeout(120) do
+        while !is_exp_started
+            sleep(1)
+            puts "=== Query the server ==="
+            res = Net::HTTP.get_response(uri)
+            content = JSON.parse(res.body)
+            is_exp_started = content['details']['is_exp_started']
+            puts "=== Status of the experimentation : #{is_exp_started} ==="
+            puts content
+        end
+    end
+
+    if !is_exp_started
+      puts "=== Somme clients are not well initialized : Experience will start anyway==="
+      res = Net::HTTP.get_response(URI("http://#{server['address']}:#{SERVER_PORT}/forceexp"))
+      content = JSON.parse(res.body)
+      puts content
     end
 
     puts "=== The expreimentation should last #{duration} secondes"
@@ -170,4 +189,10 @@ Distem.client do |cl|
     puts "=== Recovering of results ==="
     `scp root@#{server['address']}:/home/* #{SAVE_DIR}`
     puts "=== Results are saved in #{SAVE_DIR} ==="
+
+    puts "=== Get contents ==="
+    clients.each do |client|
+          puts "=== Retrive contents from #{client['name']} ==="
+          `scp root@#{client['address']}:/home/*.txt #{SAVE_DIR}`
+    end
 end
